b4e2c2e86569bf0066f800d64dac9c33cb380041
broezer@gmail.com <broezer@gmail.com>
2014-01-10 10:47:18 +0100

    Adjusted Video source

--- js/bigvideo.js
+++ js/bigvideo.js
@@ -1,5 +1,10 @@
 $(function() {
       BV = new $.BigVideo({useFlashForFirefox:false, container:$('#splash')});
 			BV.init();
+			
+			if (Modernizr.touch) {
+			    BV.show('images/static.png');
+			} else {
 			BV.show('vids/pinkyrose.mp4', {altSource:'vids/river.ogv', ambient:true});
+		}
 });
