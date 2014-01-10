66300cf268b2d0c22fd72da3599345bfac7f97d6
broezer@gmail.com <broezer@gmail.com>
2014-01-10 11:15:11 +0100

    ipad Test

--- js/bigvideo.js
+++ js/bigvideo.js
@@ -1,9 +1,10 @@
 $(function() {
-      BV = new $.BigVideo({useFlashForFirefox:false, container:$('#splash')});
+      BV = new $.BigVideo({container:$('#splash')});
 			BV.init();
+			BV.getPlayer().width(1).height(1);
 			if (Modernizr.touch) {
 			    BV.show('images/static.png');
 			} else {
-			BV.show('vids/pinkyrose.mp4', {altSource:'vids/river.ogv', ambient:true});
+					BV.show('vids/pinkyrose.mp4', {altSource:'vids/river.ogv', ambient:true});
 		}
 });
