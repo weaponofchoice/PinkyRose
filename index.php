a1f17c32c12e98eb03f686f2f9ce4d79cf5d986a
broezer@gmail.com <broezer@gmail.com>
2014-01-23 13:56:45 +0100

    iPad test

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ on the arrival of pinky rose fresh &amp; serious syrups</p>
     <footer class="row fixed">
 		<?php
 		$detect = new Mobile_Detect;
-		if ( $detect->isMobile() ) :
+		if ( $detect->isMobile() && !$detect->isTablet()) :
 		    // Do something for only mobile users
 		
 		?>
